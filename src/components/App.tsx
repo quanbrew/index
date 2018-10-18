db1daa3429ece023953534bf04cebd215e57f6aa
Quan Brew <t@ioover.net>
2018-10-18 11:50:23 +0800

    refactor and reformat code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,22 +6,21 @@ import { List } from "immutable";
 import { empty } from "../utils";
 
 
-export type Keys = List<number>;
+export type Path = List<number>;
 
 
 interface State {
   root: Item;
-  selected?: Keys;
+  selected?: Path;
 }
 
-
-// interface Context {
-// }
-// export const {Provider, Consumer} = React.createContext<Context>({});
 
 const rootKeys = List();
 
 class App extends React.Component<{}, State> {
+  edit = (selected?: Path) => this.setState({ selected });
+  update = (root: Item, callback?: () => void) => this.setState({ root }, callback);
+
   constructor(props: {}) {
     super(props);
     const root = addChild(
@@ -31,24 +30,20 @@ class App extends React.Component<{}, State> {
         createItem('hello, world')
       ),
     );
-    this.state = {root};
+    this.state = { root };
   }
 
-  edit = (selected?: Keys) => this.setState({selected});
-
-  update = (root: Item, callback?: () => void) => this.setState({root}, callback);
-
   public render() {
-    const {root, selected} = this.state;
+    const { root, selected } = this.state;
 
     return (
       <main className="App">
         <ul>
-        <ItemContainer
-          item={root} update={this.update} select={this.edit} create={empty} zoom
-          selected={selected} keys={rootKeys} next={rootKeys} prev={rootKeys}
-          indent={empty} unIndent={empty}
-        />
+          <ItemContainer
+            item={ root } update={ this.update } select={ this.edit } create={ empty }
+            selected={ selected } path={ rootKeys } next={ rootKeys } prev={ rootKeys }
+            indent={ empty } unIndent={ empty }
+          />
         </ul>
       </main>
     );
--- src/components/ItemContainer.tsx
+++ src/components/ItemContainer.tsx
@@ -1,20 +1,19 @@
 import * as React from 'react';
 import { addChild, createItem, Item } from "../Item";
-import { Keys } from "./App";
+import { Path } from "./App";
 
 
 interface Props {
   item: Item,
   update: (next: Item, callback?: () => void) => void;
-  select: (keys?: Keys) => void;
-  create: (item?: Item, position?: number) => void;
-  selected?: Keys;
-  keys: Keys;
-  next: Keys;
-  prev: Keys;
-  zoom?: boolean;
-  indent: (keys: Keys) => void;
-  unIndent: (keys: Keys) => void;
+  select: (path?: Path) => void;
+  create: (item?: Item, after?: number) => void;
+  selected?: Path;
+  path: Path;
+  next: Path;
+  prev: Path;
+  indent: (path: Path) => void;
+  unIndent: (path: Path) => void;
 }
 
 
@@ -22,131 +21,167 @@ interface State {
 }
 
 
+const itemTail = (path: Path, item: Item): Path => {
+  const last = item.children.last(null);
+  if (last === null) return path;
+  else return itemTail(path.push(item.children.size - 1), last);
+};
+
+
 export class ItemContainer extends React.PureComponent<Props, State> {
-  constructor(props: Props) {
-    super(props);
-  }
-
-  createChild = (child = createItem(), after?: number) => {
-    const {keys, item, select, update} = this.props;
+  createChildItem = (child = createItem(), after?: number) => {
+    const { path, item, select, update } = this.props;
     const size = item.children.size;
-    if (after === undefined) after = size;
-    else {
-      after += 1;
-      if (after > size) after = size;
+    let position = size;
+    if (after !== undefined) {
+      if (after + 1 < size) position = after + 1;
     }
-
-    select(keys.push(after));
-    update(addChild(item, child, after));
+    update(addChild(item, child, position), () => select(path.push(position)));
   };
+  indentChild = (path: Path) => {
+    const index = path.last(0);
 
-  itemTail = (keys: Keys, item: Item): Keys => {
-    const last = item.children.last(null);
-    if (last === null) return keys;
-    else return this.itemTail(keys.push(item.children.size - 1), last);
-  };
+    // first item or root item can't indent
+    if (index === 0) return;
 
-
-  indent = (keys: Keys) => {
-    const key = keys.last(null);
-    if (key === null || key <= 0) return;
     const { item, update, select } = this.props;
-    const prevItem = item.children.get(key - 1, null);
-    const indentItem = item.children.get(key, null);
-    if (prevItem === null || indentItem === null)
-      return console.error('unexpected key', key);
-    const children = item.children.set(key - 1, addChild(prevItem, indentItem)).remove(key);
+    const prevItem = item.children.get(index - 1, null);
+    const indentItem = item.children.get(index, null);
+    // make type checker happy
+    if (prevItem === null || indentItem === null) return console.error('unexpected index', index);
+
+    // move indent item as previous item's child.
+    const children = item.children
+      .splice(index - 1, 2, addChild(prevItem, indentItem));
+
+    // update children
     update(
-      {...item, children},
-      () => select(this.props.keys.push(key - 1, prevItem.children.size))
+      { ...item, children },
+      () => select(this.props.path.push(index - 1, prevItem.children.size))
     );
   };
+  unIndentChild = (path: Path) => {
+    // root's children can't un-indent
+    if (path.size < 2) return;
 
-
-  unIndent = (keys: Keys) => {
-    const key = keys.last(null);
-    if (key === null || keys.size < 2) return;
-    const {item, update, create} = this.props;
-    const current = item.children.get(key);
-    if (current === undefined) return console.error('unexpected key', key);
+    const index = path.last() as number;
+    const { item, update, create } = this.props;
+    const currentItem = item.children.get(index);
+    // make type checker happy
+    if (currentItem === undefined) return console.error('unexpected index', index);
     update(
-      {...item, children: item.children.remove(key)},
-      () => create(current, this.props.keys.last())
+      { ...item, children: item.children.remove(index) },
+      () => create(currentItem, this.props.path.last())
     );
   };
+  displayChild = (currentItem: Item, index: number) => {
+    const { selected, select, path, update, item, prev, next } = this.props;
 
-  items = (currentItem: Item, key: number) => {
-    const {selected, select, keys, update, item, prev, next} = this.props;
     let itemPrev = prev;
-    const prevItem = item.children.get(key - 1);
-    if (key === 0) itemPrev = keys;
+    const prevItem = item.children.get(index - 1);
+
+    if (index === 0) itemPrev = path; // move to parent
     else if (prevItem !== undefined)
-      itemPrev = this.itemTail(keys.push(key - 1), prevItem);
-    let itemNext = next;
-    if (key < item.children.size - 1) itemNext = keys.push(key + 1);
+      itemPrev = itemTail(path.push(index - 1), prevItem);
+
+    const itemNext = index < item.children.size - 1 ? path.push(index + 1) : next;
+
+    const itemPath = path.push(index);
+
+    const itemUpdate: typeof update = (next, callback) =>
+      update({ ...item, children: item.children.set(index, next) }, callback);
 
     return (
+
       <ItemContainer
         item={currentItem} key={currentItem.id} selected={selected}
-        select={select} keys={keys.push(key)}
+        select={select} path={itemPath}
         prev={itemPrev} next={itemNext}
-        create={this.createChild}
-        indent={this.indent} unIndent={this.unIndent}
-        update={(next, callback) => update({ ...item, children: item.children.set(key, next) }, callback)}
+        create={ this.createChildItem }
+        indent={ this.indentChild } unIndent={ this.unIndentChild }
+        update={ itemUpdate }
       />
     );
   };
-
-
-  content = () => {
-    const {keys, item, select} = this.props;
-    return (<div className='itemContent' onClick={() => select(keys)}>{item.text}</div>);
-  };
-
-  editing = () => (
-    <input autoFocus value={this.props.item.text} onChange={e => {
-      const next = {...this.props.item, text: e.currentTarget.value};
-      this.props.update(next);
-    }} />
-  );
-
-  down = () => {
-    const {select, next, item, keys} = this.props;
-    if (item.children.size !== 0)
-      select(keys.push(0));
-    else
-      select(next);
-  };
-
-
-  clearEditing = () => { this.props.select() };
-
   handleKeyDown = (e: React.KeyboardEvent) => {
     // console.log(e.key, e.keyCode);
-    const {indent, unIndent, create, keys, select, prev} = this.props;
-    e.stopPropagation();
+    const { item, indent, unIndent, create, path, select, prev } = this.props;
+    e.stopPropagation(); // don't propagate to parent.
     switch (e.key) {
-      case 'ArrowUp': return select(prev);
-      case 'ArrowDown': return this.down();
-      case 'Enter': return create(createItem(), keys.last());
-      case 'Escape': return this.clearEditing();
+      case 'ArrowUp':
+        return select(prev);
+      case 'ArrowDown':
+        return this.down();
+      case 'Enter':
+        // if content is empty and item is last item in siblings, indent it.
+        if (item.text === '' && this.isLastItem() && path.size > 1)
+          return unIndent(path);
+        else
+          return create(createItem(), path.last());
+      // exit edit
+      case 'Escape':
+        return this.props.select();
+      // indent or un-indent
       case 'Tab':
         e.preventDefault();
-        if (e.shiftKey) { return unIndent(keys) }
-        else { return indent(keys) }
+        if (e.shiftKey) {
+          return unIndent(path)
+        }
+        else {
+          return indent(path)
+        }
     }
   };
 
+  constructor(props: Props) {
+    super(props);
+    this.state = {};
+  }
+
+  content() {
+    const { path, item, select } = this.props;
+    return (<div className='itemContent' onClick={ () => select(path) }>{ item.text }</div>);
+  };
+
+  editing() {
+    const { item, update } = this.props;
+    return (
+      <input
+        autoFocus
+        value={ item.text }
+        onChange={ e => update({ ...item, text: e.currentTarget.value }) }
+      />
+    );
+  }
+
+  down() {
+    const { select, next, item, path } = this.props;
+    if (item.children.size !== 0)
+      select(path.push(0)); // enter next level
+    else if (this.isFinallyItem())
+      return;
+    else
+      select(next);
+  }
+
+  isLastItem(): boolean {
+    const { next, path } = this.props;
+    return path.size > next.size;
+  }
+
+  isFinallyItem(): boolean {
+    const { next, path } = this.props;
+    return !path.isEmpty() && next.isEmpty();
+  };
 
   render() {
-    const {selected, keys} = this.props;
-    const {children} = this.props.item;
-    const isSelected = selected ? selected.equals(keys) : false;
+    const { selected, path, item } = this.props;
+    const isSelected = selected ? selected.equals(path) : false;
     return (
       <li onKeyDown={this.handleKeyDown}>
-        {isSelected ? this.editing() : this.content() }
+        { isSelected ? this.editing() : this.content() }
         <ul>
-          {children.map(this.items)}
+          { item.children.map(this.displayChild) }
         </ul>
       </li>
     );
