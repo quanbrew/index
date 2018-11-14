cd0d22d6ee5279a8f4b7b95372382bcbd6a2c1db
Koppa <t@ioover.net>
2018-11-14 11:32:38 +0800

    breadcrumb style

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { findItemByPath, Item, itemLinkLocation, Path } from "../item";
 import { Link } from "react-router-dom";
+import "./Breadcrumb.css"
 
 
 interface Props {
@@ -13,32 +14,32 @@ interface State {
 }
 
 
-const makeLi = (item: Item, path: Path) => (
-  <li>
-    <Link to={ itemLinkLocation(item.id, path) }>
-      { item.editor.getCurrentContent().getPlainText() }
-    </Link>
-  </li>
-);
-
-
-export class Breadcrumb extends React.Component<Props, State> {
+export class Breadcrumb extends React.PureComponent<Props, State> {
   render() {
     const { path, root } = this.props;
     if (path.isEmpty())
       return null;
     let paths = [];
-    for (let i = 0; i < path.size; i++) {
+    for (let i = 1; i < path.size; i++) {
       paths.push(path.slice(0, i));
     }
     const list = paths.map(path => {
       const item = findItemByPath(root, path);
       if (item === null)
         throw Error("can't found item by path");
-      return makeLi(item, path);
+      return (
+        <li className="Breadcrumb-item">
+          <Link to={ itemLinkLocation(item.id, path) }>
+            { item.editor.getCurrentContent().getPlainText() }
+          </Link>
+        </li>
+      );
     });
     return (
-      <ol>{ list }</ol>
+      <ol className="Breadcrumb">
+        <li className="Breadcrumb-item Breadcrumb-root"><Link to="/">Home</Link></li>
+        { list }
+      </ol>
     );
   }
 } 
