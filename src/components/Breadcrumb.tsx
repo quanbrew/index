7e908f3f119b04d20bdc07d1cc9b38e0e5a9e222
Koppa <t@ioover.net>
2018-11-28 01:10:22 +0800

    cut of breadcrumb link
    and refactoring...

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -3,6 +3,8 @@ import { Item, Path } from "../item";
 import { Link } from "react-router-dom";
 import "./Breadcrumb.css"
 
+const ReactMarkdown = require("react-markdown");
+
 
 interface Props {
   root: Item;
@@ -15,30 +17,43 @@ interface State {
 
 
 export class Breadcrumb extends React.PureComponent<Props, State> {
+  static source(item: Item) {
+    const source = item.source;
+    const max = 24;
+    if (source.length <= max) {
+      return source;
+    }
+    else {
+      return source.substring(0, max - 1).concat("…");
+    }
+  }
+
+  item = (path: Path, key: number) => {
+    const item = Item.findByPath(this.props.root, path);
+    if (item === null)
+      throw Error("can't found item by path");
+    return (
+      <li key={ key } className="Breadcrumb-item">
+        <Link to={ Item.linkLocation(item.id, path) }>
+          <ReactMarkdown allowedTypes={ ['text'] } unwrapDisallowed source={ Breadcrumb.source(item) }/>
+        </Link>
+      </li>
+    );
+  };
+
   render() {
-    const { path, root } = this.props;
-    if (path.isEmpty())
+    const { path } = this.props;
+    if (path.isEmpty()) {
       return null;
+    }
     let paths = [];
     for (let i = 1; i < path.size; i++) {
       paths.push(path.slice(0, i));
     }
-    const list = paths.map((path, key) => {
-      const item = Item.findByPath(root, path);
-      if (item === null)
-        throw Error("can't found item by path");
-      return (
-        <li key={ key } className="Breadcrumb-item">
-          <Link to={ Item.linkLocation(item.id, path) }>
-            { item.source }
-          </Link>
-        </li>
-      );
-    });
     return (
       <ol className="Breadcrumb">
         <li className="Breadcrumb-item Breadcrumb-root"><Link to="/">Home</Link></li>
-        { list }
+        { paths.map(this.item) }
       </ol>
     );
   }
